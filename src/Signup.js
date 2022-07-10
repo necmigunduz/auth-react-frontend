1d24c56757a983d0d4f5de33a5cb914f1342ef67
necmigunduz <necmigunduz@gmail.com>
2022-07-10 09:34:27 +0300

    Add handlesubmit and onChange

--- src/Signup.js
+++ src/Signup.js
@@ -1,28 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, Button } from "react-bootstrap";
 
 function Signup() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    // prevent the form from refreshing the whole page
+    e.preventDefault();
+    // make a popup alert showing the "submitted" text
+    alert("Submited");
+  };
+  
   return (
     <>
       <h2 style={{ marginTop: "50px", fontWeight: "900" }}>Signup</h2>
-      <Form>
+      <Form onSubmit={(e) => handleSubmit(e)}>
         <Form.Group controlId="formBasicUsername">
           <Form.Label>Username</Form.Label>
-          <Form.Control type="text" placeholder="Enter your username" />
+          <Form.Control
+            type="text"
+            placeholder="Enter your username"
+            name="username"
+            value="username"
+            onChange={(e)=> setUsername(e.target.value)}
+          />
         </Form.Group>
         <Form.Group controlId="formBasicEmail">
           <Form.Label>Email adress</Form.Label>
-          <Form.Control type="email" placeholder="Enter your email adress" />
+          <Form.Control
+            type="email"
+            placeholder="Enter your email adress"
+            name="email"
+            value="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </Form.Group>
         <Form.Group controlId="formBasicPhone">
           <Form.Label>Phone number</Form.Label>
-          <Form.Control type="number" placeholder="Enter your phone number" />
+          <Form.Control
+            type="number"
+            placeholder="Enter your phone number"
+            name="phone"
+            value="phone"
+            onChange={(e) => setPhone(e.target.value)}
+          />
         </Form.Group>
         <Form.Group controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            name="password"
+            value="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </Form.Group>
-        <Button variant="primary" type="submit" style={{ marginTop: "20px" }}>
+        <Button variant="primary" type="submit" onClick={(e) => handleSubmit(e)} style={{ marginTop: "20px" }}>
           Submit
         </Button>
       </Form>
